31aebe60a6a79f03760dbd5af467ea31e0739cc1
Rany ElHousieny <elhousie@gmail.com>
2022-04-05 10:29:04 -0700

    passing props

--- src/root.component.js
+++ src/root.component.js
@@ -1,3 +1,10 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+export default function Root(
+  props
+) {
+  return (
+    <section>
+      {props.name} is mounted!{' '}
+      By {props.myProp}
+    </section>
+  );
 }
